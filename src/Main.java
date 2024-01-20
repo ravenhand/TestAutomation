fac7756bef52fbd659cd9e5888b180170464e1a9
ravenhand <edwardbnovak91@gmail.com>
2024-01-20 17:50:49 +0100

    added first testing

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         WebDriver driver = new FirefoxDriver();
-        driver.get("https://totalcar.hu/");
-        System.out.println("Hello world!");
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        WebElement userNameInput = driver.findElement(By.id("user-name"));
+        userNameInput.sendKeys("standard_user");
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys("secret_sauce");
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
