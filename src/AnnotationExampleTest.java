cc930c955564d09dfdf8a507676aa77ef82dcbc9
ravenhand <edwardbnovak91@gmail.com>
2024-01-25 19:27:03 +0100

    class name changed

--- src/AnnotationExampleTest.java
+++ src/AnnotationExampleTest.java
@@ -1,6 +1,6 @@
 import org.testng.annotations.*;
 
-public class AnnotationExample {
+public class AnnotationExampleTest {
     @BeforeSuite
     public void beforeSuite() {
         System.out.println("Before suite");
